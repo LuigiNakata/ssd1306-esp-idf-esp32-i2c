9e56d3cccaf8ebb077ca2890714d8b2d6051bdc6
LuigiNakata <luiginakata@usp.br>
2025-06-25 16:31:07 -0300

    receiver_final

--- build/esp-idf/ieee802154/cmake_install.cmake
+++ build/esp-idf/ieee802154/cmake_install.cmake
@@ -2,7 +2,7 @@
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ssd1306_esp32")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/ssd1306-esp-idf-esp32-i2c")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- main/main.c
+++ main/main.c
@@ -1,3 +1,5 @@
+#include "driver/twai.h"  // Include the TWAI (CAN) driver library
+#include "esp_log.h"      // Include ESP logging library
 #include <string.h>
 #include "driver/gpio.h"
 #include "driver/i2c.h"
@@ -5,7 +7,6 @@
 #include "esp_log.h"
 #include "freertos/task.h"
 #include "sdkconfig.h"
-#include "driver/twai.h"
 #include "freertos/FreeRTOS.h"
 
 #include "ssd1306.h"
@@ -26,9 +27,10 @@
 #define OLED_CMD_DISPLAY_ON 0xAF
 #define OLED_CMD_SET_CONTRAST 0x81
 
-#define TAG1 "TWAI"
 
-static const char *TAG = "SSD1306";
+#define TAG "CAN_DRIVER"  // Define a tag for logging messages
+
+static const char *TAG1 = "SSD1306";
 
 void i2c_master_init()
 {
@@ -63,9 +65,9 @@ void ssd1306_init() {
 
     ret = i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
     if (ret == ESP_OK) {
-        ESP_LOGI(TAG, "OLED configurado com sucesso");
+        ESP_LOGI(TAG1, "OLED configurado com sucesso");
     } else {
-        ESP_LOGE(TAG, "Erro ao configurar OLED: 0x%.2X", ret);
+        ESP_LOGE(TAG1, "Erro ao configurar OLED: 0x%.2X", ret);
     }
 
     i2c_cmd_link_delete(cmd);
@@ -92,22 +94,33 @@ void task_ssd1306_display_clear(void *ignore) {
     vTaskDelete(NULL);
 }
 
-void task_ssd1306_display_text(void *arg_text) {
-    const char *text = (const char *)arg_text;
+void ssd1306_display_text(const char *text) {
     uint8_t text_len = strlen(text);
     i2c_cmd_handle_t cmd;
     uint8_t cur_page = 0;
 
-    // Inicializa posição do cursor
+    // Clear screen
+    uint8_t zero[128] = {0};
+    for (uint8_t i = 0; i < 8; i++) {
+        cmd = i2c_cmd_link_create();
+        i2c_master_start(cmd);
+        i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_SINGLE, true);
+        i2c_master_write_byte(cmd, 0xB0 | i, true);
+        i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
+        i2c_master_write(cmd, zero, sizeof(zero), true);
+        i2c_master_stop(cmd);
+        i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
+        i2c_cmd_link_delete(cmd);
+    }
+
     cmd = i2c_cmd_link_create();
     i2c_master_start(cmd);
     i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
     i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
-    i2c_master_write_byte(cmd, 0x00, true); // reset coluna baixa
-    i2c_master_write_byte(cmd, 0x10, true); // reset coluna alta
-    i2c_master_write_byte(cmd, 0xB0 | cur_page, true); // reset página
-
+    i2c_master_write_byte(cmd, 0x00, true); 
+    i2c_master_write_byte(cmd, 0x10, true); 
+    i2c_master_write_byte(cmd, 0xB0 | cur_page, true); 
     i2c_master_stop(cmd);
     i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
@@ -118,12 +131,10 @@ void task_ssd1306_display_text(void *arg_text) {
             cmd = i2c_cmd_link_create();
             i2c_master_start(cmd);
             i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
             i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_CMD_STREAM, true);
             i2c_master_write_byte(cmd, 0x00, true);
             i2c_master_write_byte(cmd, 0x10, true);
             i2c_master_write_byte(cmd, 0xB0 | cur_page, true);
-
             i2c_master_stop(cmd);
             i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
             i2c_cmd_link_delete(cmd);
@@ -131,87 +142,100 @@ void task_ssd1306_display_text(void *arg_text) {
             cmd = i2c_cmd_link_create();
             i2c_master_start(cmd);
             i2c_master_write_byte(cmd, (OLED_I2C_ADDRESS << 1) | I2C_MASTER_WRITE, true);
-
             i2c_master_write_byte(cmd, OLED_CONTROL_BYTE_DATA_STREAM, true);
             i2c_master_write(cmd, font8x8_basic_tr[(uint8_t)text[i]], 8, true);
-
             i2c_master_stop(cmd);
             i2c_master_cmd_begin(I2C_NUM_0, cmd, 10 / portTICK_PERIOD_MS);
             i2c_cmd_link_delete(cmd);
         }
     }
-
-    vTaskDelete(NULL);
 }
 
-void app_main(void)
-{
+
+void app_main() {
+
     i2c_master_init();
     ssd1306_init();
 
-    float temperatura = 0.0f;
-    float umidade = 0.0f;
-    float velocidade_vento = 0.0f;
+    int rpm_recebido = 0;
+    int temperatura_recebida = 0;
+    int umidade_recebida = 0;
+    float temperatura_final = 0.0;
 
-    char display_text[64];
+   char display_text[64];
     snprintf(display_text, sizeof(display_text),
-         "Temp: %.2f C\nUmid: %.2f %%\nVel: %.2f m/s", temperatura, umidade, velocidade_vento);
-
-    xTaskCreate(&task_ssd1306_display_clear, "ssd1306_clear", 2048, NULL, 6, NULL);
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-
-    xTaskCreate(&task_ssd1306_display_text, 
-            "ssd1306_text", 2048, (void *)display_text, 6, NULL);
-
-      // Configure TWAI driver
-    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_21, GPIO_NUM_22, TWAI_MODE_NORMAL);
-    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();  // Set to 200 kbps
-    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();  // Accept all messages
+         "Temp: %.1f C\nUmid: %d %%\nVel: %d m/s", temperatura_final, umidade_recebida, rpm_recebido);
 
 
-    // Install TWAI driver
-    esp_err_t err = twai_driver_install(&g_config, &t_config, &f_config);
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG1, "TWAI driver installed successfully");
+    // Configure the CAN driver parameters (RX = 4; TX = 5)
+    twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT(GPIO_NUM_4, GPIO_NUM_5, TWAI_MODE_NORMAL);
+    twai_timing_config_t t_config = TWAI_TIMING_CONFIG_500KBITS();
+    twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+
+    // Initialize the CAN driver
+    if (twai_driver_install(&g_config, &t_config, &f_config) == ESP_OK) {
+        ESP_LOGI(TAG, "TWAI driver installed successfully");
     } else {
-        ESP_LOGE(TAG1, "Failed to install TWAI driver: %s", esp_err_to_name(err));
-        return;
+        ESP_LOGE(TAG, "Failed to install TWAI driver");
+        return; // Exit the function if driver installation fails
     }
 
-
-    // Start TWAI driver
-    err = twai_start();
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG1, "TWAI driver started successfully");
+    // Start the CAN driver
+    if (twai_start() == ESP_OK) {
+        ESP_LOGI(TAG, "TWAI driver started successfully");
     } else {
-        ESP_LOGE(TAG1, "Failed to start TWAI driver: %s", esp_err_to_name(err));
-        return;
+        ESP_LOGE(TAG, "Failed to start TWAI driver");
+        return; // Exit the function if driver start fails
     }
 
+    while (1){
 
-    while (1) {
-    twai_message_t message;
-    esp_err_t err = twai_receive(&message, pdMS_TO_TICKS(1000));
-    if (err == ESP_OK) {
-        ESP_LOGI(TAG1, "Message received: ID=0x%lu, Data=[%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X]",
-                    message.identifier,
-                    message.data[0], message.data[1], message.data[2], message.data[3],
-                    message.data[4], message.data[5], message.data[6], message.data[7],
-                    message.data[8], message.data[9], message.data[10], message.data[11]);
+        // Receive a message
+        twai_message_t rx_message;  // Declare a message structure for receiving
+        if (twai_receive(&rx_message, pdMS_TO_TICKS(2000)) == ESP_OK) {
+            printf("Message received -> ");
+            if (rx_message.identifier == 0x001){
+                    printf("Anemometro: \n");
+                    ESP_LOGI(TAG, "DLC: %d, Data:", rx_message.data_length_code);
+                for (int i = 0; i < rx_message.data_length_code; i++) {
+                    ESP_LOGI(TAG, "Data[%d]: %d", i, rx_message.data[i]);
+                }
+                rpm_recebido =rx_message.data[0];
+                ESP_LOGI(TAG, "RPM recebido: %d", rpm_recebido);
 
-        memcpy(&temperatura, &message.data[0], sizeof(float));
-        memcpy(&umidade, &message.data[4], sizeof(float));
-        memcpy(&velocidade_vento, &message.data[8], sizeof(float));
+            } else if (rx_message.identifier == 0x002){
+                    printf("Sensor de temperatura: \n");
+                    ESP_LOGI(TAG, "DLC: %d, Data:", rx_message.data_length_code);
+                for (int i = 0; i < rx_message.data_length_code; i++) {
+                    ESP_LOGI(TAG, "Data[%d]: %d", i, rx_message.data[i]);
+                }
+                temperatura_recebida =rx_message.data[0];
+                ESP_LOGI(TAG, "Temperatura recebida: %d", temperatura_recebida);
+                temperatura_final = temperatura_recebida / 10.0f;
+                ESP_LOGI(TAG, "Temperatura final: %.1f", temperatura_final);
 
-        ESP_LOGI(TAG1, "Temperatura = %.2f °C, Umidade = %.2f %%, Velocidade do vento = %.2f m/s",
-         temperatura, umidade, velocidade_vento);
+            } else if (rx_message.identifier == 0x003){
+                    printf("Sensor de umidade: \n");
+                    ESP_LOGI(TAG, "DLC: %d, Data:", rx_message.data_length_code);
+                for (int i = 0; i < rx_message.data_length_code; i++) {
+                    ESP_LOGI(TAG, "Data[%d]: %d", i, rx_message.data[i]);
+                }
+                umidade_recebida =rx_message.data[0];
+                ESP_LOGI(TAG, "Umidade recebida: %d%%", umidade_recebida);
+            } 
 
+        } else {
+            ESP_LOGE(TAG, "Failed to receive message");
+        }
+        snprintf(display_text, sizeof(display_text),"Temp: %.1f C\nUmid: %d %%\nVel: %d m/s", temperatura_final, umidade_recebida, rpm_recebido);
 
-    } else if (err == ESP_ERR_TIMEOUT) {
-        ESP_LOGW(TAG1, "Reception timed out");
-    } else {
-        ESP_LOGE(TAG1, "Message reception failed: %s", esp_err_to_name(err));
+        ssd1306_display_text(display_text);
+
+        printf("\n");
     }
-    }
 
+    // Stop and uninstall the CAN driver
+    twai_stop();
+    twai_driver_uninstall();
+    ESP_LOGI(TAG, "TWAI driver uninstalled");
 }
